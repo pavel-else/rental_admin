b10ae77eba2fc9422cab76f82ba01201c62318ba
pavel-else <de.pavel.se@yandex.ru>
2018-11-21 23:59:38 +0300

    Dashboard.vue - view, make navigation

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,49 +1,46 @@
-<template>
-  <div class="animated fadeIn">
-    <b-card>
-      <b-row>
-        <b-col sm="5">
-          <h4 id="traffic" class="card-title mb-0">Статистика проката</h4>
-        </b-col>
-        <b-col sm="7" class="d-none d-md-block">
-          <!-- <date-panel name="count" @setDate="setDate($event)"></date-panel> -->
-        </b-col>
-      </b-row>
+ <template>
+  <div class="stat animated fadeIn">
+    <StatNav @select="selectNavItem($event)"></StatNav>  
+    <b-card class="stat__card">
+      <h4>Статистика проката</h4>
+      <div class="stat__charts">        
+        <div v-show="selected === 'rentals'">
+          <date-panel name="count" @setDate="setDate($event)"></date-panel>
+          <div>Стартов проката</div>
+          <div class="from-to-line"><small>{{rent.count.from}}</small><small>{{rent.count.to}}</small></div>
+          <rent-count 
+            chartId="main-chart-01"
+            class="chart-wrapper" 
+            style="height:150px;margin:10px 0 40px;"
+            :type="rent.count.type"
+            :from="rent.count.from"
+            :to="rent.count.to"
+          ></rent-count>
 
-<!--       <div>Стартов проката</div>
-      <div class="from-to-line"><small>{{rent.count.from}}</small><small>{{rent.count.to}}</small></div>
-      <rent-count 
-        chartId="main-chart-01"
-        class="chart-wrapper" 
-        style="height:150px;margin:10px 0 40px;"
-        :type="rent.count.type"
-        :from="rent.count.from"
-        :to="rent.count.to"
-      ></rent-count>
+          <div>Выручка в день</div>
+          <date-panel name="cash" @setDate="setDate($event)"></date-panel>
+          <rent-cash-proceed-chart 
+            chartId="main-chart-03"
+            class="chart-wrapper" 
+            style="height:150px;margin:10px 0 40px;" 
+            :type="rent.cash.type"
+            :from="rent.cash.from"
+            :to="rent.cash.to"
+          ></rent-cash-proceed-chart>
+          <div>Всего</div>
 
-      <div>Выручка в день</div>
-      <date-panel name="cash" @setDate="setDate($event)"></date-panel>
-      <rent-cash-proceed-chart 
-        chartId="main-chart-03"
-        class="chart-wrapper" 
-        style="height:150px;margin:10px 0 40px;" 
-        :type="rent.cash.type"
-        :from="rent.cash.from"
-        :to="rent.cash.to"
-      ></rent-cash-proceed-chart>
-      <div>Всего</div> -->
-
-      <date-panel name="hours" @setDate="setDate($event)"></date-panel>
-      <div>Часов проката в день</div>
-      <rent-hours-chart 
-        chartId="main-chart-02"
-        class="chart-wrapper" 
-        style="height:150px;margin:10px 0 40px;" 
-        :type="rent.hours.type"
-        :from="rent.hours.from"
-        :to="rent.hours.to"
-      ></rent-hours-chart>
-
+          <date-panel name="hours" @setDate="setDate($event)"></date-panel>
+          <div>Часов проката в день</div>
+          <rent-hours-chart 
+            chartId="main-chart-02"
+            class="chart-wrapper" 
+            style="height:150px;margin:10px 0 40px;" 
+            :type="rent.hours.type"
+            :from="rent.hours.from"
+            :to="rent.hours.to"
+          ></rent-hours-chart>        
+        </div> 
+      </div>
     </b-card>
   </div>
 </template>
@@ -54,6 +51,7 @@ import RentCount from './dashboard/RentCount'
 import RentHoursChart from './dashboard/RentHoursChart'
 import RentCashProceedChart from './dashboard/RentCashProceedChart'
 import DatePanel from './DatePanel'
+import StatNav from './StatNav'
 import {writeLog} from '@/functions/logs'
 import * as shortDate from '@/functions/date'
 
@@ -64,7 +62,8 @@ export default {
     RentCount,
     RentHoursChart,
     RentCashProceedChart,
-    DatePanel
+    DatePanel,
+    StatNav
   },
   created() {
     this.setDateForMonth('count');
@@ -73,6 +72,7 @@ export default {
   },
   data() {
     return {
+      selected: 'rentals',
       rent: {
         count: {
           type: 'Month',
@@ -93,6 +93,10 @@ export default {
     }
   },
   methods: {
+    selectNavItem(e) {
+      console.log(e);
+      this.selected = e;
+    },
     setDate (e) {
       if (!this.rent[e.name]) {
         writeLog('Dashboard.vue, setDate', 'undefined name', {name: e.name})
@@ -154,5 +158,13 @@ export default {
   .from-to-line {
     display: flex;
     justify-content: space-between;
+  }
+
+  .stat {
+    display: flex;
+    justify-content: space-between;
+  }
+  .stat__card {
+    flex-grow: 1;
   }
 </style>
---
+++ src/views/StatNav.vue
@@ -0,0 +1,48 @@
+<template>
+  <nav class="stat__nav">
+    <ul class="ul">
+      <li class="li" @click="select('rentals')">Пункты проката</li>
+      <li class="li" @click="select('employers')">Сотрудники</li>
+      <li class="li" @click="select('products')">Товары</li>
+    </ul>
+  </nav>  
+</template>
+
+<script>
+
+export default {
+  name: 'StatNav',
+
+  data() {
+    return {
+      selected: 'rentals',
+    }
+  },
+  methods: {
+    select(msg) {
+      this.selected = msg;
+      this.$emit('select', msg);
+    }
+  }
+
+}
+</script>
+<style scoped>
+  .stat__nav {
+    outline: 1px solid lightgray;
+  }
+  .stat__nav .ul {
+    margin: 0 30px 0 0;
+    padding: 0;
+    list-style: none;
+  }
+  .stat__nav .li {
+    margin: 5px 0;
+  }
+  .stat__nav .li:hover {
+    cursor: pointer;
+  }
+  .stat__card {
+    flex-grow: 1;
+  }
+</style>
